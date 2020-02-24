c4e135d031b77a4ab75082e548c3357995d4147b
Forrest Miller <Forrestmillerj@gmail.com>
2020-02-23 20:40:28 -0600

    Responsive design changes. changed the output to be inside of a text area for more accurate passwords and easier copy and pasting. added footer. small styling changes.

--- script.js
+++ script.js
@@ -1,19 +1,3 @@
-// GIVEN I need a new, secure password
-// WHEN I click the button to generate a password
-// THEN I am presented with a series of prompts for password criteria
-// WHEN prompted for password criteria
-// THEN I select which criteria to include in the password
-// WHEN prompted for the length of the password
-// THEN I choose a length of at least 8 characters and no more than 128 characters
-// WHEN prompted for character types to include in the password
-// THEN I choose lowercase, uppercase, numeric, and/or special characters
-// WHEN I answer each prompt
-// THEN my input should be validated and at least one character type should be selected
-// WHEN all prompts are answered
-// THEN a password is generated that matches the selected criteria
-// WHEN the password is generated
-// THEN the password is either displayed in an alert or written to the page
-
 // // establish variables.
 var p;
 
@@ -27,8 +11,6 @@ function check(){
         var u = document.getElementById("upperCase").checked;
         var n = document.getElementById("numbers").checked;
         var s = document.getElementById("special").checked;
-        console.log(len)
-        console.log(l,u,n,s);
         return p = [len,l,u,n,s]
     }
 }
@@ -39,12 +21,6 @@ function main(){
     var setUpperCase = p[2]
     var setNumbers = p[3]
     var setSpecial = p[4]
-    console.log("-------------");
-    console.log(setLength)
-    console.log(setLowerCase)
-    console.log(setUpperCase)
-    console.log(setNumbers)
-    console.log(setSpecial)
 
 // function to generate password given above values.
 function generatePassword(){
@@ -61,40 +37,28 @@ function generatePassword(){
     // checks if variables are true. If variables are true they are added to the parameter array.
     if(setLowerCase === true){
         parametersArray.push(lowerAlphabet)
-        console.log(lowerAlphabet);
     }
     if(setUpperCase === true){
         parametersArray.push(upperAlphabet)
-        console.log(upperAlphabet);
     }
     if(setNumbers === true){
         parametersArray.push(number)
-        console.log(number);
     }
     if(setSpecial === true){
         parametersArray.push(special)
-        console.log(special);
     }
-    console.log("---------");
-
-    // logs the newely made parameters array after being pushed to
-    console.log(parametersArray)
 
     // creates a string of ALL the selected parameters that can be looped through in the next step.
     var parameters = parametersArray.join("")
-    console.log("joined - " + parameters)
 
     // generates characters for password by looping through the password array equal to the set length
     for(var i = 0; i < length; i++){
         passwordArray.push(parameters[Math.floor(Math.random() * parameters.length)]);
     }
-    // logs newly made password array.
-    console.log(passwordArray);
 
     // joins characters in Password array to form a password
     var password = passwordArray.join("");
-    console.log(password);
-    document.getElementById("passField").innerHTML = password;
+    document.getElementById("passFieldArea").innerHTML = password;
     // document.write(password);
 }
     // runs password function.
