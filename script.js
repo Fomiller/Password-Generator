e00ac75d01a710c805e4d9c4faebb19049d9259b
Forrest <Forrestmillerj@gmail.com>
2020-04-10 09:44:18 -0500

    moved the main function inside the if else statment in check()

--- script.js
+++ script.js
@@ -1,17 +1,20 @@
 // // establish variables.
 var p;
-
 function check(){
+    const x = document.getElementById("length").value;
     // check length is greater than 8 and less than 128
-    if( len < 8 || len > 128){
+    if( x < 8 || x > 128){
+        console.log(x);
         confirm("Choose a number between 8 and 128.")
-    }else{
+        return
+    } else {
         var len = document.getElementById("length").value;
         var l = document.getElementById("lowerCase").checked;
         var u = document.getElementById("upperCase").checked;
         var n = document.getElementById("numbers").checked;
         var s = document.getElementById("special").checked;
-        return p = [len,l,u,n,s]
+        p = [len,l,u,n,s]
+        main()
     }
 }
 
@@ -22,48 +25,47 @@ function main(){
     var setNumbers = p[3]
     var setSpecial = p[4]
 
-// function to generate password given above values.
-function generatePassword(){
-    password = []
-    passwordArray = []
-    parametersArray = []
+    // function to generate password given above values.
+    function generatePassword(){
+        password = []
+        passwordArray = []
+        parametersArray = []
 
-    var lowerAlphabet = "abcdefghijklmnopqrstuvwxyz";
-    var upperAlphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    var number = "0123456789";
-    var special = "!#$%&')*(+,-./:;=>?@[]^_`{|}~"
-    var length = setLength;
-    
-    // checks if variables are true. If variables are true they are added to the parameter array.
-    if(setLowerCase === true){
-        parametersArray.push(lowerAlphabet)
-    }
-    if(setUpperCase === true){
-        parametersArray.push(upperAlphabet)
-    }
-    if(setNumbers === true){
-        parametersArray.push(number)
-    }
-    if(setSpecial === true){
-        parametersArray.push(special)
-    }
+        var lowerAlphabet = "abcdefghijklmnopqrstuvwxyz";
+        var upperAlphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        var number = "0123456789";
+        var special = "!#$%&')*(+,-./:;=>?@[]^_`{|}~"
+        var length = setLength;
+        
+        // checks if variables are true. If variables are true they are added to the parameter array.
+        if(setLowerCase === true){
+            parametersArray.push(lowerAlphabet)
+        }
+        if(setUpperCase === true){
+            parametersArray.push(upperAlphabet)
+        }
+        if(setNumbers === true){
+            parametersArray.push(number)
+        }
+        if(setSpecial === true){
+            parametersArray.push(special)
+        }
 
-    // creates a string of ALL the selected parameters that can be looped through in the next step.
-    var parameters = parametersArray.join("")
+        // creates a string of ALL the selected parameters that can be looped through in the next step.
+        var parameters = parametersArray.join("")
 
-    // generates characters for password by looping through the password array equal to the set length
-    for(var i = 0; i < length; i++){
-        passwordArray.push(parameters[Math.floor(Math.random() * parameters.length)]);
+        // generates characters for password by looping through the password array equal to the set length
+        for(var i = 0; i < length; i++){
+            passwordArray.push(parameters[Math.floor(Math.random() * parameters.length)]);
+        }
+
+        // joins characters in Password array to form a password
+        var password = passwordArray.join("");
+        document.getElementById("passFieldArea").innerHTML = password;
+        // document.write(password);
     }
-
-    // joins characters in Password array to form a password
-    var password = passwordArray.join("");
-    document.getElementById("passFieldArea").innerHTML = password;
-    // document.write(password);
-}
     // runs password function.
-                // side note what if i gave the function values such as generatePassword(length, lower, upper, number, special)...
+    // side note what if i gave the function values such as generatePassword(length, lower, upper, number, special)...
     generatePassword()
-
 }
 
